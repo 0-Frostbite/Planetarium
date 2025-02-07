69a36553eb56259c305ea86de9e614ea234e4ad8
0-Frostbite <neutral.zfab4@proton.me>
2025-02-07 23:43:23 +0800

    basic styling

---
+++ src/main.js
@@ -0,0 +1,14 @@
+import * as THREE from 'three';
+    
+const NavBar = document.getElementById("navbar")
+NavBar.addEventListener("mouseover",function() {
+    NavBar.style.color = '#ff0000';
+});
+NavBar.addEventListener("mouseout",function() {
+    NavBar.style.color = '#ffffff';
+});
+
+const ColorMode = document.getElementById("colormode")
+ColorMode.addEventListener("click",function() {
+    
+});
