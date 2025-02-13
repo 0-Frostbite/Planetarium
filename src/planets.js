26fcd92184a6ec811e96ac18716ec71655843eb5
0-Frostbite <neutral.zfab4@proton.me>
2025-02-13 20:42:45 +0800

    forgor files

---
+++ src/planets.js
@@ -0,0 +1,12 @@
+let Planets = {
+  Mercury: { size: 0.4, distance: 0.39, mesh: null },
+  Venus: { size: 0.9, distance: 0.72, mesh: null },
+  Earth: { size: 1, distance: 1, mesh: null },
+  Mars: { size: 0.5, distance: 1.52, mesh: null },
+  Jupiter: { size: 11, distance: 5.2, mesh: null },
+  Saturn: { size: 9.4, distance: 9.6, mesh: null },
+  Uranus: { size: 4, distance: 19.2, mesh: null },
+  Neptune: { size: 3.9, distance: 30.7, mesh: null },
+};
+
+export default Planets;
---
+++ src/test.js
@@ -0,0 +1,5 @@
+import Planets from "./planets.js";
+
+for (let i in Planets) {
+  console.log(Planets[i]);
+}
